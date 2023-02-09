6b823083e980a3be27f0a1d84c210427c7fae4e0
6matko <ace@bbit.tech>
2023-02-09 15:38:11 +0200

    Added weight upsert form fields and emitter
    - Added weight model (interface)

---
+++ src/app/modules/weight/components/weight-upsert-form/weight-upsert-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Weight } from '../../models/weight';
+
+@Component({
+  selector: 'app-weight-upsert-form',
+  templateUrl: './weight-upsert-form.component.html',
+  styleUrls: ['./weight-upsert-form.component.scss']
+})
+export class WeightUpsertFormComponent {
+  form: FormGroup;
+  @Output() weightAdded: EventEmitter<Weight> = new EventEmitter();
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  addWeight(): void {
+    this.weightAdded.emit(this.form.value);
+  }
+
+  private createForm(): void {
+    this.form = this.formBuilder.group({
+      weight: new FormControl(0, [Validators.required, Validators.min(0)]),
+      comment: new FormControl('')
+    });
+  }
+}
---
+++ src/app/modules/weight/models/weight.ts
@@ -0,0 +1,5 @@
+
+export interface Weight {
+    weight: number;
+    comment: string;
+}
