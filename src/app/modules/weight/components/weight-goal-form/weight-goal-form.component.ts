d09565357395188dcc25bc69b95ca93400e10640
6matko <ace@bbit.tech>
2023-02-09 16:59:40 +0200

    Added display of previously set goal after route change

--- src/app/modules/weight/components/weight-goal-form/weight-goal-form.component.ts
+++ src/app/modules/weight/components/weight-goal-form/weight-goal-form.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { filter, first } from 'rxjs/operators';
+import { WeightService } from '../../weight.service';
 
 @Component({
   selector: 'app-weight-goal-form',
@@ -7,10 +9,22 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./weight-goal-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeightGoalFormComponent {
+export class WeightGoalFormComponent implements OnInit {
   goalControl = new FormControl(0, [Validators.required, Validators.min(0)]);
   @Output() goalSaved: EventEmitter<number> = new EventEmitter();
-  constructor() { }
+  constructor(
+    private readonly weightService: WeightService
+  ) { }
+
+  ngOnInit(): void {
+    this.weightService.getGoal()
+      .pipe(
+        // Making ssure goal is truthy and with valid value
+        filter(goal => goal && goal >= 0),
+        first()
+      )
+      .subscribe(goal => this.goalControl.setValue(goal));
+  }
 
   saveGoal(): void {
     this.goalSaved.emit(this.goalControl.value);
