86501e6678e61639886383c8c933f7d28dceecf1
maksims.busigins <maksims.busigins@visma.com>
2023-02-27 09:07:49 +0200

    add basic binding for the main view
    progress indicator;
    goal & today forms

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  progress: number = 0;
 
+  getProgress() {
+    let progressPercent = this.progress / this.goalForm.value.goal;
+    return progressPercent;
+  }
+
+  goalForm = this.formBuilder.group({
+    goal: new FormControl(),
+  });
+
+  onSubmitGoal() {
+
+  }
+
+  todayForm = this.formBuilder.group({
+    desc: new FormControl(),
+    today: new FormControl(),
+  });
+
+  onSubmitToday() {
+    this.progress += this.todayForm.value.today;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 }
