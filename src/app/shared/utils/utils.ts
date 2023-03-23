97ae9ab3aecf7889ac944248559f6e39ab53cb75
Serhii Hrudakov <avatarcapa@gmail.com>
2023-03-23 08:49:37 +0000

    Extended utils with formatDate function

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,4 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+
 export function kgToLbs(kg: number): number {
   return kg * 2.20462;
 }
@@ -9,4 +10,13 @@ export function generateId(): string {
 
 export function removeDecimalValues(value: string): string {
   return parseFloat(value.replace(',', '.')).toFixed(3);
+}
+
+export function formatDate(milliseconds: number): string {
+  const date = new Date(milliseconds);
+  const dateStr = date.toDateString();
+
+  const [_, month, dayOfMonth, year] = dateStr.split(' ');
+
+  return `${month} ${dayOfMonth}, ${year}`;
 }
