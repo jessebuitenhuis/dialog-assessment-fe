b0af71fd69798659eab02319c6f2ae27c057b8a0
Serhii Hrudakov <avatarcapa@gmail.com>
2023-03-24 11:37:08 +0000

    Subscribe to isMetric and weight props. update and re-generate titles on WeightViewItemComponent

--- src/app/features/weight-viewer/components/weight-view-item/weight-view-item.component.ts
+++ src/app/features/weight-viewer/components/weight-view-item/weight-view-item.component.ts
@@ -1,22 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {formatDate, kgToLbs} from '../../../../shared/utils/utils';
-import {getWeightUnit} from '../../../../shared/utils/textUtils';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { formatDate, kgToLbs } from '../../../../shared/utils/utils';
+import { getWeightUnit } from '../../../../shared/utils/textUtils';
 
 @Component({
   selector: 'app-weight-viewer-item',
   template: `
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">{{title}}</h5>
-        <h6 class="card-subtitle fw-lighter mb-2 text-muted">Created at {{dateCreated}}</h6>
-        <p class="card-text">{{description}}</p>
+        <h5 class="card-title">{{ title }}</h5>
+        <h6 class="card-subtitle fw-lighter mb-2 text-muted">
+          Created at {{ dateCreated }}
+        </h6>
+        <p class="card-text">{{ description }}</p>
         <a [href]="id" class="card-link">Edit</a>
       </div>
     </div>
   `,
 })
-
-export class WeightViewItemComponent implements OnInit {
+export class WeightViewItemComponent implements OnInit, OnChanges {
   @Input() id: string;
   @Input() weight: number;
   @Input() description: string;
@@ -31,5 +38,17 @@ export class WeightViewItemComponent implements OnInit {
 
     this.title = `${weight} ${getWeightUnit(this.isMetric)}`;
     this.dateCreated = formatDate(Number(this.created));
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.isMetric || changes.weight) {
+      this.handleIsMetricAndWeightChange();
+    }
+  }
+
+  handleIsMetricAndWeightChange(): void {
+    const weight = this.isMetric ? this.weight : kgToLbs(this.weight);
+
+    this.title = `${weight} ${getWeightUnit(this.isMetric)}`;
   }
 }
