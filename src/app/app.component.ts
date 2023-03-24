c49170473bd36b6a4dd76d6a754d554a3cb8c9f0
Serhii Hrudakov <avatarcapa@gmail.com>
2023-03-24 11:46:40 +0000

    Binded router and screens at AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { storageKeys } from '../environments/environment';
+import { retreatFromStorage } from './shared/utils/storageUtils';
+import { UserSettings } from '../types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dialog-assessment-fe';
+export class AppComponent implements OnInit {
+  targetWeight: number;
+
+  private readonly storageKey = storageKeys.userSettings;
+
+  ngOnInit(): void {
+    const userData = retreatFromStorage<UserSettings>(this.storageKey);
+
+    this.targetWeight = userData.targetWeight;
+  }
 }
