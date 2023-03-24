669d680fe3513a6776eff6eeaf8e861df507c180
Serhii Hrudakov <avatarcapa@gmail.com>
2023-03-24 11:25:36 +0000

    Subscribe shared userSettings on edit-screen.component.ts - Prettier

--- src/app/screens/edit-screen/edit-screen.component.ts
+++ src/app/screens/edit-screen/edit-screen.component.ts
@@ -1,22 +1,52 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SharedService } from '../../core/services/shared.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-edit-screen',
   template: `
-    <app-weight-edit-view [id]="id" [isMetric]="isMetric"></app-weight-edit-view>
+    <app-weight-edit-view
+      [id]="id"
+      [isMetric]="isMetric"
+    ></app-weight-edit-view>
   `,
 })
-export class EditScreenComponent implements OnInit {
+export class EditScreenComponent implements OnInit, OnDestroy {
   id: string;
 
-  @Input() isMetric: boolean;
+  isMetric: boolean;
 
-  constructor(private route: ActivatedRoute) {}
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
     });
+
+    this.route.params
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((params) => {
+        if (params.id) {
+          this.id = params.id;
+        }
+      });
+
+    this.sharedService.userSettings$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((userSettings) => {
+        this.isMetric = userSettings.isMetric;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
