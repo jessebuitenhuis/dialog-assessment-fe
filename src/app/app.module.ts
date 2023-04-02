a708537966313d41069ccd3f76e79d7887d26a4d
Ioannis <psaradakisj@gmail.com>
2023-04-02 20:37:15 +0300

    feat: Weight Chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
 import { FormsModule } from '@angular/forms';
 import { GoalSettingComponent } from './goal-setting/goal-setting.component';
 import { NgModule } from '@angular/core';
 import { UnitSwitcherComponent } from './unit-switcher/unit-switcher.component';
+import { WeightChartComponent } from './weight-chart/weight-chart.component';
 import { WeightEntryComponent } from './weight-entry/weight-entry.component';
 import { WeightHistoryComponent } from './weight-history/weight-history.component';
 
@@ -14,8 +16,9 @@ import { WeightHistoryComponent } from './weight-history/weight-history.componen
     WeightHistoryComponent,
     GoalSettingComponent,
     UnitSwitcherComponent,
+    WeightChartComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/weight.service.ts
+++ src/app/weight.service.ts
@@ -1,3 +1,5 @@
+import { BehaviorSubject, Observable } from 'rxjs';
+
 import { Injectable } from '@angular/core';
 
 export interface WeightEntry {
@@ -12,14 +14,17 @@ export interface WeightEntry {
 export class WeightService {
   weightEntries: WeightEntry[];
   goal: number | null;
+  private entryAddedSource: BehaviorSubject<WeightEntry[]>;
 
   constructor() {
     this.weightEntries = [];
     this.goal = null;
+    this.entryAddedSource = new BehaviorSubject<WeightEntry[]>([]);
   }
 
   addEntry(entry: WeightEntry): void {
     this.weightEntries.push(entry);
+    this.entryAddedSource.next(this.weightEntries);
   }
 
   setGoal(goal: number): void {
@@ -33,5 +38,9 @@ export class WeightService {
     const latestWeight =
       this.weightEntries[this.weightEntries.length - 1].weight;
     return latestWeight - this.goal;
+  }
+
+  getEntryAddedObservable(): Observable<WeightEntry[]> {
+    return this.entryAddedSource.asObservable();
   }
 }
