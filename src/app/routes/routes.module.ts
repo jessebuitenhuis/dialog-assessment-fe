27e23017bc419f1dc0c32a2c6bee3cd2b9300dc5
Mohsen <m.mehabadi@gmail.com>
2023-11-16 16:46:32 +0100

    completed the dashboard,and stored data

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -6,9 +6,19 @@ import { RoutesRoutingModule } from './routes-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ShareModule } from '../share/share.module';
 import { EntryComponent } from './components/entry/entry.component';
+import { GoalEntryComponent } from './components/goal/goal-entry.component';
+import { StoreService } from './services/store.service';
+import { ChartComponent } from './components/chart/chart.component';
+import { StatusComponent } from './components/status/status.component';
 
 @NgModule({
-  declarations: [DashboardComponent, EntryComponent],
+  declarations: [
+    DashboardComponent,
+    EntryComponent,
+    GoalEntryComponent,
+    ChartComponent,
+    StatusComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -17,4 +27,8 @@ import { EntryComponent } from './components/entry/entry.component';
   ],
   providers: [],
 })
-export class RoutesModule {}
+export class RoutesModule {
+  constructor(private storeService: StoreService) {
+    this.storeService.load();
+  }
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CardComponent } from './components/card/card.component';
+import { GoalInputComponent } from './components/goal-input/goal-input.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
-const ELEMENTS = [CardComponent];
+const ELEMENTS = [CardComponent, GoalInputComponent, LineChartComponent];
 
 @NgModule({
   declarations: [...ELEMENTS],
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, NgApexchartsModule],
   exports: [...ELEMENTS],
 })
 export class ShareModule {}
